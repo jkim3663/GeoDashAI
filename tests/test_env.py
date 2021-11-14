53f5179907db9366229b1b4e2e3a09fba4ea39c6
Travis Cho <chotravis87@gmail.com>
2021-11-14 19:52:09 +0900

    Update test_env.py

--- tests/test_env.py
+++ tests/test_env.py
@@ -13,8 +13,7 @@ def driver(request):
 class EnvTest(unittest.TestCase):
     def test_env_init(self):
         assert self.env.driver.current_url == 'https://games-online.io/game/Geometry_Jump/'
-        assert self.env.driver.get_window_size()['width'] == 1024 and \
-            self.env.driver.get_window_size()['height'] == 768
     
     def test_env_observation(self):
-        assert self.env.observation.shape == self.env.observation_space.shape
+        assert self.env.observation.shape[0] == self.env.observation_space.shape[0] and \
+            self.env.observation.shape[1] == self.env.observation_space.shape[1]
