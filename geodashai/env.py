d3d6cdfd8a881e8284b9310d58e8d9ed1204bc8e
Jun Yeop Kim <junyeopkim0205@gmail.com>
2021-12-03 22:44:15 -0500

    Update env.py
    def done() value update

--- geodashai/env.py
+++ geodashai/env.py
@@ -92,8 +92,7 @@ class GeometryDash(gym.Env):
     def done(self, img: Optional[np.array]=None) -> bool:
         if img:
             return not np.any(img.squeeze(-1)[:,210:240] == 186)
-        # return not np.any(self.observation.squeeze(-1)[:,210:240] == 186)
-        return np.any(np.squeeze(self.observation)[:,210:240] == 186)
+        return np.any(np.squeeze(self.observation)[:,240:280] == 179)
     
     def retry_clickable(self, img: Optional[np.array]=None) -> bool:
         if img:
